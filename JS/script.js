4673382770c02228e4a3e0a44b741b84dc113457
DEVASHISH7777 <63115818+DEVASHISH7777@users.noreply.github.com>
2020-05-08 21:02:44 +0200

    Add files via upload

--- JS/script.js
+++ JS/script.js
@@ -1,71 +1,259 @@
 "use strict";
 
+function chargement(){
+    appelProc()
+    appelCM()
+    appelMV()
+    appelCG()
+    appelDD()
+    appelBoitier()
+}
 
-function appelProc(){
+function nomUtilisateur(){
+    
+}
 
+
+/* appel processeur*/
+function appelProc() {
     let xhr = new XMLHttpRequest();
-    xhr.open('get', "processeurs" , true);
-    xhr.onload =  chargementProc ();
+    xhr.open("get", "processeurs", true);
+    xhr.onload = chargementProc;
+    xhr.responseType = "json";
     xhr.send();
     return false;
 }
 
-function chargementProc(){
+function chargementProc() {
+    let reponse = this.response;
+    console.log(reponse);
 
-    let reponse = JSON.parse(this.responseText);
-    console.log(reponse)
-    let procNom=""
-    let procPrix=""
+    let procId = "";
+    for (let i of reponse) {
+        let giveId = "processeurId" + i.ProcesseurId;
 
+        procId += '<div class="containerProduits" id="' + giveId + '" onclick="choisirComposant()">' +  /* onClock ne functione pas */
+            '<div class="intContainerProduits">' +
+            '<div class="nomDescriptionProd">' +
+            '<div class="monProd">' +
+            '<p>' + i.ProcesseurName + '</p > ' +
+            '</div>' +
+            '<div class="descriptionProd">' +
+            '<p> ' + i.socket + '</p>' +
+            '</div>' +
+            '</div>' +
+            '<img src="" alt="" /*TODO add ploto name drom database OPTIONAL*/>' +
+            '</div>' +
+            '<div class="intContainerPrix">' +
+            '<p>' + i.ProcesseurPrix + '</p>' +
+            '</div>' +
+            '</div>';
+    };
+    document.getElementById("processeur").insertAdjacentHTML('beforeend', procId);
 
-    for(let i of reponse){
+}
 
-        procNom += "<div id=\""+i+"\">" + i.ProcesseurName + "</div><br>"
-        procPrix += "<div id=\""+i+"\">" + i.ProcesseurPrix + "</div><br>"
+/***************************************************************************************************************appel carte mere */
 
-
-    }
-
-    document.getElementById("productsProc").innerHTML = procNom
-    document.getElementById("priceProc").innerHTML = procPrix
+function appelCM() {
+    let xhr = new XMLHttpRequest();
+    xhr.open("get", "carteMere", true);
+    xhr.onload = chargementCM;
+    xhr.responseType = "json";
+    xhr.send();
+    return false;
 }
 
 
 function chargementCM() {
-    appelCM()
     let reponse = this.response;
-    let Nom = ""
-    let Prix = ""
+    console.log(reponse);
 
+    let procId = "";
+    for (let i of reponse) {
+        let giveId = "carteMereId" + i.CarteMereId;
 
-   /* for (let i of reponse) {
-
-        Nom += "<div id=\"" + i + "\">" + i.CarteMereName + "</div><br>"
-        Prix += "<div id=\"" + i + "\">" + i.CarteMerePrix + "</div><br>"
-    }*/
-
-    document.getElementById("productsCm").innerHTML = Nom
-    document.getElementById("priceCm").innerHTML = Prix
+        procId += '<div class="containerProduits" id="' + giveId + '" onclick="choisirComposant()">' +  /* onClock ne functione pas */
+            '<div class="intContainerProduits">' +
+            '<div class="nomDescriptionProd">' +
+            '<div class="monProd">' +
+            '<p>' + i.CarteMereName + '</p > ' +
+            '</div>' +
+            '<div class="descriptionProd">' +
+            '<p> ' + i.socket + '</p>' +
+            '</div>' +
+            '</div>' +
+            '<img src="" alt="" /*TODO add ploto name drom database OPTIONAL*/>' +
+            '</div>' +
+            '<div class="intContainerPrix">' +
+            '<p>' + i.CarteMerePrix + '</p>' +
+            '</div>' +
+            '</div>';
+    };
+    document.getElementById("carteMere").insertAdjacentHTML('beforeend', procId);
 
 }
 
-function appelCM(){
+/********************************************************************************************************************appel memoire vive */
 
+function appelMV() {
     let xhr = new XMLHttpRequest();
-    xhr.open('get', "carteMere" , true);
-    xhr.onload = chargementCM ;
+    xhr.open("get", "ram", true);
+    xhr.onload = chargementMV;
     xhr.responseType = "json";
     xhr.send();
+    return false;
 }
 
 
+function chargementMV() {
+    let reponse = this.response;
+    console.log(reponse);
 
-/*function remplire(){
-    var procDonnees = document.getElementById("processeur").value
-    procDonnees.innerHTML
+    let procId = "";
+    for (let i of reponse) {
+        let giveId = "ramId" + i.RamId;
 
-}*/
+        procId += '<div class="containerProduits" id="' + giveId + '" onclick="">' +  /* onClock ne functione pas */
+            '<div class="intContainerProduits">' +
+            '<div class="nomDescriptionProd">' +
+            '<div class="monProd">' +
+            '<p>' + i.RamName + '</p > ' +
+            '</div>' +
+            '<div class="descriptionProd">' +
+            '<p> ' + i.capacite + '</p>' +
+            '</div>' +
+            '</div>' +
+            '<img src="" alt="" /*TODO add ploto name drom database OPTIONAL*/>' +
+            '</div>' +
+            '<div class="intContainerPrix">' +
+            '<p>' + i.RamPrix + '</p>' +
+            '</div>' +
+            '</div>';
+    };
+    document.getElementById("memoireVive").insertAdjacentHTML('beforeend', procId);
+
+}
+
+/*******************************************************************************************************appel carte graphique */
+
+function appelCG() {
+    let xhr = new XMLHttpRequest();
+    xhr.open("get", "carteGraphique", true);
+    xhr.onload = chargementCG;
+    xhr.responseType = "json";
+    xhr.send();
+    return false;
+}
 
 
+function chargementCG() {
+    let reponse = this.response;
+    console.log(reponse);
+
+    let procId = "";
+    for (let i of reponse) {
+        let giveId = "carteGraphiqueId" + i.CarteGraphiqueId;
+
+        procId += '<div class="containerProduits" id="' + giveId + '" onclick="choisirComposant()">' +  /* onClock ne functione pas */
+            '<div class="intContainerProduits">' +
+            '<div class="nomDescriptionProd">' +
+            '<div class="monProd">' +
+            '<p>' + i.CarteGraphiqueName + '</p > ' +
+            '</div>' +
+            '<div class="descriptionProd">' +
+            '<p> ' + i.memoire + '</p>' +
+            '</div>' +
+            '</div>' +
+            '<img src="" alt="" /*TODO add ploto name drom database OPTIONAL*/>' +
+            '</div>' +
+            '<div class="intContainerPrix">' +
+            '<p>' + i.CarteGraphiquePrix + '</p>' +
+            '</div>' +
+            '</div>';
+    };
+    document.getElementById("carteGraphique").insertAdjacentHTML('beforeend', procId);
+
+}
+/*****************************************************************************************************************appel disque dur */
+
+function appelDD() {
+    let xhr = new XMLHttpRequest();
+    xhr.open("get", "disqueDur", true);
+    xhr.onload = chargementDD;
+    xhr.responseType = "json";
+    xhr.send();
+    return false;
+}
 
 
+function chargementDD() {
+    let reponse = this.response;
+    console.log(reponse);
+
+    let procId = "";
+    for (let i of reponse) {
+        let giveId = "disqueDurId" + i.DisqueDurId;
+
+        procId += '<div class="containerProduits" id="' + giveId + '" onclick="">' +  /* onClick ne functione pas */
+            '<div class="intContainerProduits">' +
+            '<div class="nomDescriptionProd">' +
+            '<div class="monProd">' +
+            '<p>' + i.DisqueDurName + '</p > ' +
+            '</div>' +
+            '<div class="descriptionProd">' +
+            '<p> ' + i.capacite + '</p>' +
+            '</div>' +
+            '</div>' +
+            '<img src="" alt="" /*TODO add ploto name drom database OPTIONAL*/>' +
+            '</div>' +
+            '<div class="intContainerPrix">' +
+            '<p>' + i.DisqueDurPrix + '</p>' +
+            '</div>' +
+            '</div>';
+    };
+    document.getElementById("disqueDur").insertAdjacentHTML('beforeend', procId);
+
+}
+
+
+/***************************************************************************************************************appel boitier */
+
+function appelBoitier() {
+    let xhr = new XMLHttpRequest();
+    xhr.open("get", "boitier", true);
+    xhr.onload = chargementBoitier;
+    xhr.responseType = "json";
+    xhr.send();
+    return false;
+}
+
+
+function chargementBoitier() {
+    let reponse = this.response;
+    console.log(reponse);
+
+    let procId = "";
+    for (let i of reponse) {
+        let giveId = "boitierId" + i.BoitierId;
+
+        procId += '<div class="containerProduits" id="' + giveId + '" onclick="choisirComposant()">' +  /* onClock ne functione pas */
+            '<div class="intContainerProduits">' +
+            '<div class="nomDescriptionProd">' +
+            '<div class="monProd">' +
+            '<p>' + i.BoitierName + '</p > ' +
+            '</div>' +
+            '<div class="descriptionProd">' +
+            '<p> ' + 'Langeur:' + i.langeur + 'hauteur:' + i.hauteur + '</p>' +
+            '</div>' +
+            '</div>' +
+            '<img src="" alt="" /*TODO add ploto name drom database OPTIONAL*/>' +
+            '</div>' +
+            '<div class="intContainerPrix">' +
+            '<p>' + i.BoitierPrix + '</p>' +
+            '</div>' +
+            '</div>';
+    };
+    document.getElementById("boitier").insertAdjacentHTML('beforeend', procId);
+
+}
--- JS/scriptAcceuil.js
+++ JS/scriptAcceuil.js
@@ -1,47 +1,45 @@
 "user strict";
 
-let idLoginBlock = document.getElementById("loginPage");
-let idSigninBlock = document.getElementById("signinPage");
-
-function switchToSignin() {
-  idLoginBlock.style.display = "none";
-  idSigninBlock.style.display = "flex";
-}
-
-function switchToLognin() {
-  idLoginBlock.style.display = "flex";
-  idSigninBlock.style.display = "none";
-}
 
 function enregistrePseudo(formulaire) {
 
   let usr =  document.getElementById("pseudo").value;
- // localStorage.setItem('pseudo', usr)
   let xhr = new XMLHttpRequest();
-  //verifePseudo(this)
   xhr.open('GET','enregistrer_nom?usr=' + usr,true);
   xhr.send();
-
-  alert("vous avez bien été enregistrer vous pouvez passé a la page suivante");
   return false;
+
 }
 
 
+function traiterPseudo(){
 
-/*function verifePseudo(this){
   let pseudo =  document.getElementById("pseudo").value;
   let xhr = new XMLHttpRequest();
-  xhr.open('GET','check_pseudo?usr=' + pseudo,true);
-  onload = function(this){
-    if(pseudo == this.response){
-      alert("ce pseudo est deja prit vous etes bien " + pseudo );
-    }
-
-  }
+  xhr.open('GET','check_pseudo?usr=' + pseudo ,true);
+  xhr.onload = verifePseudo;
   xhr.send();
-  return false
+
+  return false;
 
 }
 
-*/
+function verifePseudo(){
+
+  let reponse = this.response;
+
+  console.log(reponse)
+    if( reponse == 1 ){
+      alert("Ce pseudo existe deja , vous etes bien ? : " + document.getElementById("pseudo").value + " Si non trouvé un autre pseudo")
+    }
+
+    if( reponse == 0 ){
+      enregistrePseudo()
+      alert("vous avez bien été enregistré vous pouvez passer à la page suivante");
+    }
+}
+
+
+
+
 
